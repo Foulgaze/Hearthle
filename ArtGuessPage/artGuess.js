fd8895bc9011cfea1be298a486017aecf63444e9
Foulgaze <gaberighi@gmail.com>
2025-03-24 20:14:05 -0500

    Added Art Guess. Art starts at high zoom and lowers with each guess

---
+++ ArtGuessPage/artGuess.js
@@ -0,0 +1,50 @@
+
+import { GuessController } from '../guessingGrid/guessController.js'
+import { setupDropdown } from '../dropdown/dropdown.js'
+var zoomLevels = [1500, 1000, 750, 500, 300, 250, 175, 100, 100]
+var currentZoomLevel = 0
+function setupCardArt()
+{
+	let fullArt = document.getElementById("fullArt")
+	let zoomPercentage = document.getElementById("zoomPercentage")
+	let zoomLevel = zoomLevels[currentZoomLevel++]
+	fullArt.src = window.guessController.targetCard['fullArt']
+	fullArt.style.transform = `scale(${zoomLevel}%)`;
+	zoomPercentage.innerHTML = `${zoomLevel}%<br>Zoom`
+	setupDropdown(function(name){window.guessController.cardHasBeenGuessed(name)},window.guessController.searchBar)
+	console.log("Setting zoom")
+}
+
+function onWin()
+{
+	document.getElementById("imageHolder").style.display = "none"
+	document.getElementById("zoomPercentageHolder").style.display = "none"
+}
+
+function onGuess()
+{
+	let fullArt = document.getElementById("fullArt")
+	let zoomPercentage = document.getElementById("zoomPercentage")
+	let zoomLevel = zoomLevels[currentZoomLevel++]
+	fullArt.style.transform = `scale(${zoomLevel}%)`;
+	zoomPercentage.innerHTML = `${zoomLevel}%<br>Zoom`
+	console.log("Updating Zoom to ", zoomLevel)
+
+}
+
+
+function init()
+{
+	setupCardArt()
+}
+function main()
+{
+	const cardNameFilePath = "../Assets/CardData/art.txt"
+	const searchHeaders = ["name", "manaCost", "rarity", "classes", "power", "toughness", "minionType"]
+	const col = 7
+	const row = 8
+	const cookiePath = "ArtGuessPage"
+	const shareMessageHeader = "I solved today's Hearthle (Based on Art) in "
+	window.guessController = new GuessController(cardNameFilePath, init, searchHeaders, grid, col, row, onGuess,onWin,cookiePath, shareMessageHeader)
+}
+main()
